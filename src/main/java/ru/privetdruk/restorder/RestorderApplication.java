93147def964ea122b2156917fb32c866da7c6e80
Somsin <privetdruk.inc@gmail.com>
2022-01-18 19:58:20 +0300

    Initial commit

---
+++ src/main/java/ru/privetdruk/restorder/RestorderApplication.java
@@ -0,0 +1,13 @@
+package ru.privetdruk.restorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestorderApplication.class, args);
+	}
+
+}
