f9140cc3cd599a78d4e53ac1d7b881f8d0a98498
Sergey Savrukhin <savrawar@mail.ru>
2022-03-19 17:47:37 +0300

    fix dev build

--- src/main/java/ru/privetdruk/restorder/bot/UserBot.java
+++ src/main/java/ru/privetdruk/restorder/bot/UserBot.java
@@ -13,7 +13,7 @@ import ru.privetdruk.restorder.service.UserBotService;
 
 import javax.annotation.PostConstruct;
 
-@Component
+
 @RequiredArgsConstructor
 @Slf4j
 public class UserBot extends TelegramWebhookBot {
