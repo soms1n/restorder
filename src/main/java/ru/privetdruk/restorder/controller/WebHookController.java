11248fa7c3c17a5c42b3323badaa9a9235278dc2
Sergey Savrukhin <savra.sv@yandex.ru>
2022-05-09 22:17:33 +0300

    deploy

--- src/main/java/ru/privetdruk/restorder/controller/WebHookController.java
+++ src/main/java/ru/privetdruk/restorder/controller/WebHookController.java
@@ -1,7 +1,6 @@
 package ru.privetdruk.restorder.controller;
 
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,24 +15,18 @@ public class WebHookController {
     private final ClientBot clientBot;
     private final UserBot userBot;
 
-    @Value("${bot.user.enabled}")
-    private boolean userBotEnabled;
-
-    public boolean isUserBotEnabled() {
-        return userBotEnabled;
-    }
-
     public WebHookController(ClientBot clientBot, UserBot userBot) {
         this.clientBot = clientBot;
         this.userBot = userBot;
     }
 
-    @PostMapping(value = "/")
-    public Mono<BotApiMethod<?>> onUpdateReceived(@RequestBody Update update) {
-        if (isUserBotEnabled()) {
-            return Mono.just(userBot.onWebhookUpdateReceived(update));
-        } else {
-            return Mono.just(clientBot.onWebhookUpdateReceived(update));
-        }
+    @PostMapping(value = "/user")
+    public Mono<BotApiMethod<?>> onUserUpdateReceived(@RequestBody Update update) {
+        return Mono.just(userBot.onWebhookUpdateReceived(update));
+    }
+
+    @PostMapping(value = "/client")
+    public Mono<BotApiMethod<?>> onClientUpdateReceived(@RequestBody Update update) {
+        return Mono.just(clientBot.onWebhookUpdateReceived(update));
     }
 }
